907e5f631130fe0f5d79754f40b67b23be896c8a
Lucas Elias de Abreu <developer.elias@outlook.com.br>
2024-05-17 22:13:54 -0300

    finaalizacao do metodo verifica se acabou

--- ControleJogo.py
+++ ControleJogo.py
@@ -9,7 +9,7 @@ class jogo:
     def jogada(self,jogador, jogo, posL, posC):
         if jogador == self.__x:
             jogoatualizado = jogo[posL[posC]] = "x"
-        else:
+        if jogador == self.__o:
             jogoatualizado = jogo[posL[posC]] = "o"
         return jogoatualizado
     
@@ -20,4 +20,43 @@ class jogo:
            return False
     
     def verificaSeAcabou(self, jogo):
-        pass
+        for linha in jogo:
+            if linha[0] == linha[1] == linha[2]:
+                return True
+        
+        diagonal = []
+        for i,linha in len(jogo),jogo:
+            for j,coluna in len(linha),linha:
+                if i==j:
+                    diagonal.append(linha[coluna])
+        if diagonal[0] == diagonal[1] == diagonal[2]:
+            return True
+        
+        diagonalInversa = []
+        for i,linha in len(jogo),jogo:
+            for j,coluna in len(linha),linha:
+                if i+j == 4:
+                    diagonalInversa.append(linha[coluna])
+        if diagonalInversa[0] == diagonalInversa[1] == diagonalInversa[2]:
+            return True
+        
+        reta1 = []
+        reta2 = []
+        reta3 = []
+        for i,linha in len(jogo),jogo:
+            for j,coluna in len(linha),linha:
+                if j == 0:
+                    reta1.append(linha[coluna])
+                if j == 1:
+                    reta2.append(linha[coluna])
+                if j == 2:
+                    reta3.append(linha[coluna])
+                
+        if reta1[0] == reta1[1] == reta1[2]:
+            return True
+        if reta2[0] == reta2[1] == reta2[2]:
+            return True
+        if reta3[0] == reta3[1] == reta3[2]:
+            return True
+        
+        return False
